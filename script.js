4858816c1d81fa23e5349520c19b5400efc64907
Robert Erickson <bobericksonjr@gmail.com>
2021-04-18 13:11:42 -0400

    adds decimal functinality

--- script.js
+++ script.js
@@ -15,28 +15,50 @@ function divide(dividend, divisor) {
 }
 
 function operate(operator, num1, num2) {
+    let result;
     switch(operator) {
         case "+":
-            return add(num1, num2);
+            result = add(num1, num2);
+            break;
         case "-":
-            return subtract(num1, num2);
+            result = subtract(num1, num2);
+            break;
         case "*":
-            return multiply(num1, num2);
+            result = multiply(num1, num2);
+            break;
         case "/":
-            return divide(num1, num2);
+            if (num2 == 0) {
+                return "Error";
+            }
+            result = divide(num1, num2);
+            break;
         default:
-            return;
+            result = null;
+            break;
     }
+    let resultString = result.toString();
+    if ( resultString.length > 13) {
+        if(!resultString.includes('.')) {
+            return "Error";
+        } else {
+            let splitNumbers = resultString.split('.')
+            console.log(splitNumbers)
+            if (splitNumbers[0].length > 12) {
+                return "Error";
+            } else {
+                let decimalPlaces = 12 - splitNumbers[0].length;
+                console.log(decimalPlaces);
+                console.log(result, typeof result)
+                return result.toFixed(decimalPlaces);
+            }
+        }
+    }
+    return result;
 }
 
-const display = document.querySelector('#display-box');
-const numberButtons = document.querySelectorAll('.numbers');
-const operatorButtons = document.querySelectorAll('.operations');
-const equals = document.querySelector('#equals');
-const clear = document.querySelector('#clear');
 
 function main() {
-    // global variables
+    // global object variable
     let dataObj = {};
 
     displayNumbers(dataObj);
@@ -48,7 +70,6 @@ function main() {
 function setClear(obj) {
     clear.addEventListener('click', () => {
     obj.displayVar = null;
-    obj.firstVar = null;
     obj.secondVar = null;
     obj.operator = null;
     obj.result = null;
@@ -59,157 +80,109 @@ function setClear(obj) {
 
 
 function displayNumbers(obj) {
+    const decimalButton = document.querySelector('#decimal');
+    decimalButton.addEventListener('click', function(e) {
+        if (display.textContent.length < 13 && !display.textContent.includes('.')
+            && !obj.result) {
+            display.textContent += '.';
+            if (!obj.displayVar) {
+                obj.displayVar = parseFloat(display.textContent);
+                console.log(obj);
+            } else {
+                obj.secondVar = parseFloat(display.textContent);
+                console.log(obj);
+            }
+        }
+    });
     console.log(obj);
     numberButtons.forEach(button => button.addEventListener('click', function(e) { 
         const clickedNumber = e.target.id;
-        if (display.textContent.length < 13) {
-            display.textContent += clickedNumber;
-            if (!obj.operator) {
-                obj.displayVar = parseInt(display.textContent);
-                console.log(obj)
-            } else {
-                obj.secondVar = parseInt(display.textContent);
+        //if (display.textContent == "Error") {
+            //display.textContent = '';
+        if (obj.result == "Error") {
+            return;
+        }
+        if (!obj.result) {
+            if (display.textContent.length < 13) {
+                display.textContent += clickedNumber;
+                if (!obj.operator) {
+                    obj.displayVar = parseFloat(display.textContent);
+                    console.log(obj)
+                } else {
+                    obj.secondVar = parseFloat(display.textContent);
+                    console.log(obj);
+                }
             }
+        } else {
+            display.textContent = '';
+            obj.result = null;
+            display.textContent += clickedNumber;
+            obj.secondVar = parseFloat(display.textContent);
+            console.log(obj);
         }
     }));
 }
 
 function setOperators(obj) {
     operatorButtons.forEach(button => button.addEventListener('click', function(e) {
-        obj.operator = e.target.id;
-        console.log(obj);
-        if (!obj.displayVar) {
+        if (obj.result == "Error") {
             return;
-        } else {
-            console.log(obj);
+        } else if (obj.result) {
+            obj.operator = e.target.id;
+            obj.displayVar = parseFloat(obj.result);
+            obj.result = null;
+            obj.secondVar = null;
             display.textContent = '';
+            console.log(obj);
+        } else if (!obj.operator) {
+            obj.operator = e.target.id;
+            console.log(obj);
+            if (!obj.displayVar) {
+                return;
+            } else {
+                console.log(obj);
+                display.textContent = '';
+            }
+        } else {
+            obj.result = operate(obj.operator, obj.displayVar, obj.secondVar);
+            display.textContent = obj.result;
+            obj.displayVar = obj.result;
+            obj.operator = e.target.id;
+            obj.secondVar = null;
+            console.log(obj);
         }
     }));
 }
 
 function setEquals(obj) {
     equals.addEventListener('click', function(e) {
-        if (!obj.secondVar) {
-            obj.secondVar = parseInt(display.textContent);
+        if (obj.result == "Error") {
+            return;
+        } else if (!obj.secondVar && !obj.result) {
+            display.textContent = "Error";
+            obj.result = "Error";
+            obj.displayVar = null;
+            obj.operator = null;
+            console.log(obj);
+            return;
+        } else if (!obj.secondVar) {
+            obj.secondVar = parseFloat(display.textContent);
             console.log(obj.secondVar);
         }
         console.log(obj);
-        obj.result = operate(obj.operator, obj.displayVar, obj.secondVar);
-        console.log(obj.result);
-        display.textContent = obj.result;
-        obj.displayVar = obj.result;
+        let operationResult = operate(obj.operator, obj.displayVar, obj.secondVar);
+        obj.result = parseFloat(operationResult);
+        console.log(obj);
+        display.textContent = obj.result.toString();
+        obj.displayVar = parseFloat(obj.result);
     });
     
 }
 
+const display = document.querySelector('#display-box');
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operations');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#clear');
 
 main();
-
-/* function setButtons() {
-    let displayValue;
-    let secondValue;
-    let operationValue;
-    let result;
-
-    const numberButtons = document.querySelectorAll('.numbers');
-    numberButtons.forEach(button => button.addEventListener('click', function(e) { 
-        const clickedNumber = e.target.id;
-        let display = document.querySelector('#display-box');
-        if (display.textContent.length < 13) {
-            display.textContent += clickedNumber;
-            if (operationValue) {
-                secondValue = parseInt(display.textContent);
-                console.log(displayValue, secondValue, operationValue)
-                const equals = document.querySelector('#equals');
-                equals.addEventListener('click', function() {
-                    console.log(displayValue, secondValue, operationValue)
-                    result = operate(operationValue, displayValue, secondValue)
-                    document.querySelector('#display-box').textContent = result;
-                    displayValue = result;
-                    secondValue = null;
-                    operationValue = null;
-                    console.log(displayValue, secondValue, operationValue)
-                });
-            } else {
-                displayValue = parseInt(display.textContent);
-            }
-        }
-    }));
-
-    const operatorButtons = document.querySelectorAll('.operations');
-    operatorButtons.forEach(button => button.addEventListener('click', function(e) {
-        document.querySelector('#display-box').textContent = '';
-        operationValue = e.target.id;
-    }));
-}
-
-function setEquals(var1, var2, operation) {
-    const equals = document.querySelector('#equals');
-    equals.addEventListener('click', function() {
-        let result = operate(operation, var1, var2)
-        document.querySelector('#display-box').textContent = result;
-    })
-}
-
-
-setButtons(); */
-
-/*
-function main() {
-    // set storage variables
-    let operationObject = {};
-
-    //set clear button
-    const clearButton = document.querySelector('#clear');
-    clearButton.addEventListener('click', () => {
-        document.querySelector('#display-box').innerText = '';
-        delete operationObject.var1;
-        delete operationObject.var2;
-        delete operationObject.operation;
-        delete operationObject.result;
-        console.log(operationObject);
-    });
-
-    setNumberButtons();
-
-    setOperationsButtons(operationObject);
-}
-
-function setNumberButtons() {
-    const numberButtons = document.querySelectorAll('.numbers');
-    numberButtons.forEach(button => button.addEventListener('click', function(e) {
-        const clickedNumber = e.target.id;
-        let display = document.querySelector('#display-box');
-        if (display.textContent.length < 13) {
-            display.textContent += clickedNumber;
-        }
-    }));
-}
-
-function setOperationsButtons(obj) {
-    const operationButtons = document.querySelectorAll('.operations');
-    operationButtons.forEach(button => button.addEventListener('click', function(e) {
-        obj.operation = e.target.id;
-        if (!document.querySelector('#display-box').innerText) {
-            return;
-        } else if (!obj.var1) {
-            obj.var1 = parseInt(document.querySelector('#display-box').innerText);
-            document.querySelector('#display-box').innerText = '';
-            obj = setEqualsButton(obj);
-        }
-    }));
-}
-
-
-function setEqualsButton(obj) {
-    console.log(obj)
-    document.querySelector('#equals').addEventListener('click', function() {
-        obj.var2 = parseInt(document.querySelector('#display-box').innerText);
-        obj.result = operate(obj.operation, obj.var1, obj.var2);
-        console.log(obj);
-        document.querySelector('#display-box').innerText = obj.result;
-        return obj;
-    });
-}
-
-main();*/
