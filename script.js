954ec5bb37c711ea5495ada6a7f7f0b96f35d620
Robert Erickson <bobericksonjr@gmail.com>
2021-04-20 14:17:54 -0400

    adds keyboard support

--- script.js
+++ script.js
@@ -73,15 +73,23 @@ function setClear(obj) {
 }
 
 function clearData(obj) {
-    obj.val1 = null;
-    obj.val2 = null;
-    obj.operator = null;
-    obj.isDisplayingResult = false;
-    obj.isEqualsClicked = false;
-    obj.isDisplayingMemory = false;
+    clearStatus(obj);
+    clearCache(obj);
     obj.memory = 0;
     display.textContent = '';
     console.log(obj);
+}
+
+function clearCache(obj) {
+    obj.val1 = null;
+    obj.val2 = null;
+    obj.operator = null;
+}
+
+function clearStatus(obj) {
+    obj.isDisplayingResult = false;
+    obj.isEqualsClicked = false;
+    obj.isDisplayingMemory = false;
 }
 
 function setNumbers(obj) {
@@ -102,21 +110,15 @@ function setNumbers(obj) {
         if (display.textContent.length < 13 && !display.textContent.includes('.')) {
             if (obj.isEqualsClicked === true ) {
                 // clear values except memory
-                obj.val1 = null;
-                obj.val2 = null;
-                obj.operator = null;
-                obj.isDisplayingResult = false;
-                obj.isEqualsClicked = false;
-                obj.isDisplayingMemory = false;
+                clearCache(obj);
+                clearStatus(obj);
                 display.textContent = '';
                 display.textContent = '.';
                 console.log(obj);
                 return;
             } else if (obj.isDisplayingResult || obj.isDisplayingMemory) {
                 display.textContent = '';
-                obj.isEqualsClicked = false;
-                obj.isDisplayingResult = false;
-                obj.isDisplayingMemory = false;
+                clearStatus(obj);
             }
             display.textContent += '.';
         }
@@ -126,12 +128,9 @@ function setNumbers(obj) {
 function displayNumber(e, obj) {
     console.log(obj);
      if (obj.isEqualsClicked === true) { // clear display & cache if displaying result
-        obj.val1 = null;
-        obj.val2 = null;
-        obj.operator = null;
+        clearCache(obj);
         display.textContent = e.target.id;
-        obj.isEqualsClicked = false;
-        obj.isDisplayingResult = false;
+        clearStatus(obj);
         console.log(obj);
         return;
     } else if (obj.isDisplayingResult) {
@@ -170,8 +169,7 @@ function setOperators(obj) {
             display.textContent = '';
             obj.val2 = null;
             obj.operator = e.target.id;
-            obj.isEqualsClicked = false;
-            obj.isDisplayingResult = false;
+            clearStatus(obj);
             console.log(obj);
         }    
     }));
@@ -201,48 +199,11 @@ function displayMemory(obj) {
     obj.isDisplayingResult = false;
     obj.isDisplayingMemory = true;
 }
-/*
-function readyOperator(e, obj) {
-    //check to see if doing consectutive operations
-    //we'll test that by seeing if there's already an inputed value,
-    //if there's an inputed operator, and a current value typed in
-    if (obj.val1 !== null && obj.operator !== null && display.textContent != '') {
-        obj.val1 = display.textContent;
-        obj.val2 = null;
-        obj.operator = e.target.id;
 
-    }
-    if (obj.continue === true) {
-        readyOperation(obj);
-        obj.operator = e.target.id;
-        console.log(obj, 125)
-        return;
-    } else if (obj.result === true) {
-        display.textContent = '';
-        obj.operator = e.target.id;
-        obj.result = false;
-        obj.val2 = null;
-        console.log(obj, 132);
-        return;
-    } else if (obj.val1) {
-        obj.val2 = parseFloat(display.textContent);
-        readyOperation(obj);
-        console.log(obj);
-        obj.operator = e.target.id;
-        obj.val2 = null;
-        obj.continue = true;
-        return;
-    }
-    obj.val1 = parseFloat(display.textContent);
-    obj.operator = e.target.id;
-    display.textContent = null;
-    console.log(obj);
-}
-*/
 function setEquals(obj) {
     const equals = document.querySelector('#equals');
     equals.addEventListener('click', function() {
-        if (display.textContent === "Error" || display.textContent == '' || !obj.val1 || !obj.operator) {
+        if (display.textContent === "Error" || display.textContent == '' || typeof obj.val1 != "number" || !obj.operator) {
             return;
         } else if (!obj.isEqualsClicked) {
             obj.val2 = parseFloat(display.textContent);
@@ -258,22 +219,6 @@ function setEquals(obj) {
         }
     });
 }
-/*
-function readyOperation(obj) {
-    if (obj.val2 == null) { // grab val2 from disply if there isn't one already stored
-    obj.val2 = parseFloat(display.textContent);
-    }
-    let result = parseFloat(operate(obj.operator, obj.val1, obj.val2)) // why do I sometimes get a string?
-    if (isNaN(result)) {
-        display.textContent = "Error";
-        return;
-    }
-    display.textContent = result;
-    obj.val1 = result;
-    obj.result = true; // true = calculator is displaying result
-    //obj.continue = false;
-    console.log(obj);
-} */
 
 setNumbers(dataObj);
 setOperators(dataObj);
@@ -281,162 +226,18 @@ setEquals(dataObj);
 setClear(dataObj);
 setMemory(dataObj);
 
-/*
-function setClear(obj) {
-    const clear = document.querySelector('#clear');
-    clear.addEventListener('click', () => {
-        obj.displayVar = null;
-        obj.secondVar = null;
-        obj.operator = null;
-        obj.result = null;
-        obj.memory = 0;
-        display.textContent = null;
-        console.log(obj);
-    });
-}
-
-function setDecimal(obj) {
-    const decimalButton = document.querySelector('#decimal');
-    decimalButton.addEventListener('click', function(e) {
-        if (display.textContent.length < 13 && !display.textContent.includes('.')
-            && obj.result == null) {
-            display.textContent += '.';
-            if (obj.displayVar == null) {
-                obj.displayVar = parseFloat(display.textContent);
-                console.log(obj);
-            } else {
-                obj.secondVar = parseFloat(display.textContent);
-                console.log(obj);
-            }
-        }
-    });
-}
-
-function displayNumbers(obj) {
-    console.log(obj);
-    const numberButtons = document.querySelectorAll('.numbers');
-    numberButtons.forEach(button => button.addEventListener('click', function(e) { 
-        const clickedNumber = e.target.id;
-        if (obj.result == "Error") { // deactivate buttons when error message
-            return;
-        }
-        if (obj.result == null) { // starting fresh
-            console.log("one") // HERE ADD LOGS TO FIGURE OUT WHICH PATH IS HAPPENINING WHEN NUMBER IS CLICKED AFTER AN OPERATION IS RESOLVED...
-            if (display.textContent.length < 13) {
-                display.textContent += clickedNumber;
-                if (!obj.operator) { // continue storing number 1
-                    obj.displayVar = parseFloat(display.textContent);
-                    console.log(obj)
-                } else { // if there's an operater, ready for number 2
-                    obj.secondVar = parseFloat(display.textContent);
-                    console.log(obj);
-                }
-            }
-        } else if (obj.displayVar != null && obj.secondVar != null && obj.operator 
-                   && obj.result != null) { //starting fresh
-            obj.displayVar = null;
-            obj.secondVar = null;
-            obj.operator = null;
-            obj.result = null;
-            display.textContent = null;
-            display.textContent = clickedNumber;
-        } else { // if there's a result, it's number 1, and we need number 2
-            display.textContent = null;
-            obj.result = null;
-            display.textContent += clickedNumber;
-            obj.secondVar = parseFloat(display.textContent);
-            console.log(obj);
-        }
-    }));
-}
-
-function setOperators(obj) {
-    const operatorButtons = document.querySelectorAll('.operations');
-    operatorButtons.forEach(button => button.addEventListener('click', function(e) {
-        if (obj.result == "Error" || obj.displayVar == null) { // ignore if no numbers
-            return;
-        } else if (obj.result) {
-            obj.operator = e.target.id;
-            obj.displayVar = parseFloat(obj.result);
-            obj.result = null;
-            obj.secondVar = null;
-            display.textContent = null;
-            console.log(obj);
-        } else if (!obj.operator) {
-            obj.operator = e.target.id;
-            console.log(obj);
-            if (obj.displayVar == null) {
-                display.textContent = null;
-                return;
-            } else {
-                console.log(obj);
-                display.textContent = null;
-            }
-        } else {
-            obj.result = parseFloat(operate(obj.operator, obj.displayVar, obj.secondVar));
-            if (isNaN(obj.result)) {
-                obj.result = "Error";
-            }
-            display.textContent = obj.result;
-            obj.displayVar = obj.result;
-            obj.operator = e.target.id;
-            obj.secondVar = null;
-            console.log(obj);
-        }
-    }));
-}
-
-function setEquals(obj) {
-    const equals = document.querySelector('#equals');
-    equals.addEventListener('click', function(e) {
-        if (obj.result == "Error" || obj.displayVar == null) { // ignore if no numbers
-            return;
-        } else if (obj.secondVar == null && obj.result == null) {
-            display.textContent = "Error";
-            obj.result = "Error";
-            obj.displayVar = null;
-            obj.operator = null;
-            console.log(obj);
-            return;
-        } else if (obj.secondVar == null) {
-            obj.secondVar = parseFloat(display.textContent);
-            console.log(obj.secondVar);
-        }
-        console.log(obj);
-        let operationResult = operate(obj.operator, obj.displayVar, obj.secondVar);
-        obj.result = parseFloat(operationResult);
-        if (isNaN(obj.result)) {
-            obj.result = "Error";
-        }
-        console.log(obj);
-        display.textContent = obj.result.toString();
-        obj.displayVar = parseFloat(obj.result);
-    });
-}
-
-function setMemory(obj) {
-    const memoryButton = document.querySelector('#memory');
-    memoryButton.addEventListener('click', () => display.textContent = obj.memory);
-
-    const memoryAdd = document.querySelector('#memory-add');
-    memoryAdd.addEventListener('click', () => obj.memory += parseFloat(display.textContent));
-
-    const memorySubtract = document.querySelector('#memory-subtract');
-    memorySubtract.addEventListener('click', () => obj.memory -= parseFloat(display.textContent));
-
-
-}
-
-const display = document.querySelector('#display-box');
-const dataObj = {
-    memory: 0,
-};
-
-
-displayNumbers(dataObj);
-setClear(dataObj);
-setOperators(dataObj);
-setEquals(dataObj);
-setDecimal(dataObj);
-setMemory(dataObj);
-*/
+// keyboard support
+document.addEventListener('keyup', (e) => {
+    let key = e.key;
+    if (key === "Enter") {
+        key = "equals";
+    } else if (key === ".") {
+        key = "decimal"
+    } else if (key === "Delete") {
+        key = "clear"
+    }
+    if (!document.getElementById(key)) {
+        return;
+    }
+    document.getElementById(key).click();
+});
